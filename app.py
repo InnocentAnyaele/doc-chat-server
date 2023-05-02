b7a950ecd3451aaa210c9dd21f52428540327e8b
InnocentAnyaele <luisnerma545@gmail.com>
2023-05-02 13:19:35 +0000

    complete first index phase

--- app.py
+++ app.py
@@ -4,8 +4,9 @@ from functools import wraps
 import os
 import uuid
 from werkzeug.utils import secure_filename
-from utils import createIndex, checkExtension, startDeleteThread
+from utils import createIndex, checkExtension, startDeleteThread, queryPineconeIndex
 from flask_cors import CORS
+import json
 
 
 def create_app(config = DevelopmentConfig()):
@@ -41,6 +42,10 @@ def create_app(config = DevelopmentConfig()):
     @token_required
     def add_data():
         if request.method == 'POST':
+            response = make_response('testIndexKey')
+            response.status_code = 200 
+            return response
+            
             try:
                 uniqueDirectoryName = str(uuid.uuid1())
                 uniqueDirName = os.path.join('./data/', uniqueDirectoryName)
@@ -69,6 +74,37 @@ def create_app(config = DevelopmentConfig()):
                     response.status_code = 400
                     print (extension, 'is not accepted')
                     return (response)
+            except Exception as e:
+                print (e)
+                response = make_response('Something went wrong')
+                response.status_code = 500
+                return response
+            
+            
+    @app.route('/api/queryIndex', methods=['POST'])
+    @token_required
+    def queryIndex():
+        if request.method == 'POST':
+            try:
+                # indexKey = request.form['indexKey']
+                indexKey = 'a8ebbf9e-e8d7-11ed-83ea-20c19bff2da4'
+                query = request.form['prompt']
+                chatHistory = json.loads(request.form['chatHistory'])
+                
+                
+                print ('this is the index key',indexKey)
+                print ('this is the chat history',chatHistory)
+                print (chatHistory[0])
+                print (chatHistory[0]['sender'])
+                print (chatHistory[0]['message'])
+                
+                output = queryPineconeIndex(chatHistory,query,indexKey)
+                
+                response = make_response(output)
+                response.status_code = 200
+                print (output)
+                return response
+                
             except Exception as e:
                 print (e)
                 response = make_response('Something went wrong')
--- utils.py
+++ utils.py
@@ -3,6 +3,7 @@ import os
 import shutil
 import threading
 from config import Config
+import json
 
 from langchain.document_loaders import UnstructuredPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
@@ -22,21 +23,21 @@ config = Config()
 
 sampleData = './data/SamplePDF.pdf'
 
-sampleChatHistory = [
-[{"human_input": "What is my name"}, {"output": "Your name is Innocent"}],[{"human_input": "How old am I"}, {"output": "I am 23"}]]
+sampleChatHistory =  [{'sender': 'system', 'message': 'Reading your document...'}, {'sender': 'system', 'message': 'Context has been uploaded. Ask away!! NB: Session will expire after 5 mins.'}, {'sender': 'user', 'message': 'test query'}, {'sender': 'AI', 'message': 'Sorry something went wrong. Try again later or restart context'}, {'sender': 'user', 'message': 'test'}, {'sender': 'system', 'message': 'Sorry something went wrong. Try again later or restart context'}, {'sender': 'system', 'message': 'Context has been cleared'}, {'sender': 'system', 'message': 'Reading your document...'}, {'sender': 'system', 'message': 'Context has been uploaded. Ask away!! NB: Session will expire after 5 mins.'}, {'sender': 'user', 'message': 'test'}, {'sender': 'AI', 'message': 'done'}, {'sender': 'user', 'message': 'test'}, {'sender': 'AI', 'message': 'done'}]
 
 
-def createIndex(path=sampleData):
-    loader = UnstructuredPDFLoader("SamplePDF.pdf")
+
+def createIndex(path):
+    loader = UnstructuredPDFLoader(path)
     data = loader.load()
     
-    print (f'You have {len(data)} document(s) in your data')
-    print (f'You have {len(data[0].page_content)} characters in your document')
+    # print (f'You have {len(data)} document(s) in your data')
+    # print (f'You have {len(data[0].page_content)} characters in your document')
     
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=50, chunk_overlap=0)
     texts = text_splitter.split_documents(data)
     
-    print (f'Now you have {len(texts)} documents')
+    # print (f'Now you have {len(texts)} documents')
     
     embeddings = OpenAIEmbeddings(openai_api_key=config.OPENAI_API_KEY)
     
@@ -50,9 +51,10 @@ def createIndex(path=sampleData):
     
     Pinecone.from_texts([t.page_content for t in texts], embeddings, index_name=newIndexName)
     
+    print (newIndexName)
     return newIndexName
 
-def queryIndex(chatHistory, query, indexKey):
+def queryPineconeIndex(chatHistory, query, indexKey):
     template = """
     You are a chatbot having a conversation with a human.
 
@@ -79,19 +81,30 @@ def queryIndex(chatHistory, query, indexKey):
     
     index = Pinecone.from_existing_index(index_name=indexKey, embedding=embeddings)
     
-    
 
-    for chat in sampleChatHistory:
-        memory.save_context(chat[0], chat[1])
-
+    for chat in chatHistory:
+        chat_message = chat['message']
+        chat_sender = chat['sender']
+        if chat_sender != 'system':     
+            if chat['sender'] == 'AI':
+                memory.chat_memory.add_ai_message(chat_message)
+            else:
+                memory.chat_memory.add_user_message(chat_message)
+        
+        
     # memory.save_context({"human_input": "What is my name"}, {"output": "Your name is Innocent"})
     # memory.save_context({"human_input": "How old am I"}, {"output": "I am 20 years old"})
 
     chain = load_qa_chain(OpenAI(temperature=0, openai_api_key=config.OPENAI_API_KEY), chain_type="stuff", memory=memory, prompt=prompt)
     docs = index.similarity_search(query, include_metadata = True)
-    chain_output = chain({"input_documents":docs, "human_input":query}, return_only_outputs=True)
+    # chain_output = chain({"input_documents":docs, "human_input":query}, return_only_outputs=True)
+    chain_output = chain({"input_documents":docs, "human_input":query})
     
-    return chain_output
+    # print (chain_output)
+    # print (type(chain_output))
+    print ('conversation history', chain.memory.buffer)
+    print (chain_output['output_text'])
+    return chain_output['output_text']
 
 def delete_context(dirName):
     time.sleep(300)
@@ -118,5 +131,6 @@ def deleteAllData():
 
 
 if __name__ == '__main__':
-    print (config.OPENAI_API_KEY)
-    
+    # createIndex(sampleData)
+    # queryPineconeIndex(sampleChatHistory,'What is this document about?','a8ebbf9e-e8d7-11ed-83ea-20c19bff2da4')
+    pass
